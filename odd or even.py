0ae0be76894347d23ddbc6c5643a2c6be92e65c5
aromal <aromalaps@gmail.com>
2023-07-20 05:23:15 -0700

    text document created

---
+++ odd or even.py
@@ -0,0 +1,6 @@
+#check value is odd or even
+value=int(input("enter a value"))
+if value%2==0:
+    print("the value is an odd number")
+else:
+    print("the value is an even number")
---
+++ posorneg.py
@@ -0,0 +1,6 @@
+#check the value is positive or negative
+value=int(input("enter a value"))
+if value > 0 :
+    print("the value is positive")
+else:
+    print("the value is negative")
